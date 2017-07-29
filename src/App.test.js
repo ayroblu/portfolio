b3226cd200829f94035e08e0964974cb88d83cd1
Ben Lu <ben@ben-l.com>
2017-07-29 22:30:46 +1200

    Fix for test

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin()
 //import renderer from 'react-test-renderer'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(
+    <MuiThemeProvider>
+      <App />
+    </MuiThemeProvider>
+  , div)
 
   //const tree = renderer.create(
   //  <App />
